9c744fe92c03b1a2d46f1fad09f7aaa64fe257b2
Alex <p.adw@gmail.com>
2022-07-07 14:38:34 +0300

    fist task

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 "use strict";
- 
+
+let numberOfFilms=[]
+numberOfFilms[0]= prompt('Сколько фильмов вы уже посмотрели','');
+let personalMovieDB ={
+    'count': numberOfFilms[0],
+    'movies': {},
+    'actors': {},
+    'genres': [],
+    'privat': 'false'
+};
+numberOfFilms[1] = prompt('Один из последних просмотренных фильмов?','');
+numberOfFilms[2] = prompt('На сколько оцените его?','');
+let movies= {
+    'lastFilm': numberOfFilms[1],
+    'marks': numberOfFilms[2]
+};
+console.log(numberOfFilms[0] + numberOfFilms[1]+ numberOfFilms[2]);
