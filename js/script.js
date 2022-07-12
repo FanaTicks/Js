d197222bb5ce7227ea78532dafc3282bbb43fb92
Alex <p.adw@gmail.com>
2022-07-12 10:40:45 +0300

    task 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 "use strict"; 
 
-const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+let numberOfFilms;
+
+function start(){
+   numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+   while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+      numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+   }
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,15 +17,42 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-forst: for(let i =0; i < 2; i++){
-   let name1 = prompt('Один із останніх переглянутих фільмів?',''),
-   grade1 = prompt('На скільки ви його оціните?','');
-if(name1 == null || grade1 == null || name1.length == 0 || grade1.length == 0 || name1.length >= 50 || grade1.length >= 50){i--; continue forst;}
-   personalMovieDB.movies[name1] = grade1;
+
+function rememberFilms(){
+   forst: for(let i =0; i < 2; i++){
+      let name1 = prompt('Один із останніх переглянутих фільмів?',''),
+      grade1 = prompt('На скільки ви його оціните?','');
+   if(name1 == null || grade1 == null || name1.length == 0 || grade1.length == 0 || name1.length >= 50 || grade1.length >= 50){i--; continue forst;}
+      personalMovieDB.movies[name1] = grade1;
+   }
 }
-if(personalMovieDB.count <10){alert('Просмотрено довольно мало фильмов');}
-else if(personalMovieDB.count >=10 && personalMovieDB.count <30){alert('Вы классический зритель');}
-else if (personalMovieDB.count >30){alert('Вы киноман');}
-else{alert('Произошла ошибка');}
-      
-console.log(personalMovieDB);
+
+rememberFilms();
+
+function detectPerconalLevel(){
+   if(personalMovieDB.count <10){alert('Просмотрено довольно мало фильмов');}
+   else if(personalMovieDB.count >=10 && personalMovieDB.count <30){alert('Вы классический зритель');}
+   else if (personalMovieDB.count >30){alert('Вы киноман');}
+   else{alert('Произошла ошибка');}  
+}
+
+detectPerconalLevel();
+
+function showMyDB(){
+   if(personalMovieDB.privat == false){
+      console.log(personalMovieDB);
+   }
+}
+
+function writeYourGenres(){
+   personalMovieDB.genres[0] = prompt('Ваш любимый жанр под номером 1', '');
+   personalMovieDB.genres[1] = prompt('Ваш любимый жанр под номером 2', '');
+   personalMovieDB.genres[2] = prompt('Ваш любимый жанр под номером 3', '');
+}
+writeYourGenres();
+showMyDB();
+
+
+
+
+
---
+++ scr.js
@@ -0,0 +1,6 @@
+"use strict"; 
+
+const srt = "dawdwa";
+const  arr = [1, 2, 4];
+
+console.log(arr.length);
